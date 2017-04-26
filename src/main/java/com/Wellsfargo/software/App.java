7ed68ec023c5487d38e736a5869f9d120714f8e8
NaveenChinnappa <naveen.chinnappa@gmail.com>
2017-04-26 09:32:15 +0530

    Adding maven

---
+++ src/main/java/com/Wellsfargo/software/App.java
@@ -0,0 +1,13 @@
+package com.Wellsfargo.software;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
